f9cb0c831457358d5e20460aa49f69fdd14fa1ba
YumiD <gacon.dorian@gmail.com>
2022-06-07 18:52:15 +0200

    Catmull Set Up Update

--- Assets/Scripts/CatmullClark/CatmullClark.cs
+++ Assets/Scripts/CatmullClark/CatmullClark.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class CatmullClark : MonoBehaviour
 {
+    Mesh _mesh;
+    Vector3[] _vertices;
+    Vector3[] _normals;
     void Start()
     {
-
+        _mesh = GetComponent<MeshFilter>().mesh;
+        _vertices = _mesh.vertices;
+        _normals = _mesh.normals;
     }
 
     void Update()
