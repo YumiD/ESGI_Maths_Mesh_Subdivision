038803f840613095d552fb3bf16c36e17c131b65
MaximeVg <49032976+DevilSky93@users.noreply.github.com>
2022-06-08 12:55:52 +0200

    Clean loop code

--- Assets/Scripts/Loop/ApplyAlgorithm.cs
+++ Assets/Scripts/Loop/ApplyAlgorithm.cs
@@ -9,11 +9,13 @@ namespace Loop
             MeshFilter filter = GetComponent<MeshFilter>();
 
             // Require a mesh to weld (require to remove duplicated vertices)
-            Mesh welded = LoopAlgorithm.Weld(filter.mesh, float.Epsilon, filter.mesh.bounds.size.x);
+            Mesh unfilteredMesh = filter.mesh;
+            Mesh filteredMesh =
+                LoopAlgorithm.RemoveDuplicateVertices(unfilteredMesh, float.Epsilon, unfilteredMesh.bounds.size.x);
 
             Mesh mesh = LoopAlgorithm.Subdivide(
-                welded, // a welded mesh
-                2, // subdivision count
+                filteredMesh, // a filtered mesh
+                1, // subdivision count
                 false // a result mesh is welded or not
             );
             filter.sharedMesh = mesh;
