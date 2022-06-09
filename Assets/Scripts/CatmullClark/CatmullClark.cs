911a0e102aa65a68e37d4dc8e5d790f07f74ef42
YumiD <gacon.dorian@gmail.com>
2022-06-09 08:59:28 +0200

    Face Detector let's go

--- Assets/Scripts/CatmullClark/CatmullClark.cs
+++ Assets/Scripts/CatmullClark/CatmullClark.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class CatmullClark : MonoBehaviour
 {
     Mesh _mesh;
-    Vector3[] _vertices;
+    /*Vector3[] _vertices;
     Vector3[] _normals;
+    int[] _triangles;*/
     void Start()
     {
-        _mesh = GetComponent<MeshFilter>().mesh;
-        _vertices = _mesh.vertices;
+        _mesh = GetComponent<MeshFilter>().sharedMesh;
+        if(_mesh == null){
+            Debug.Log("Mesh non trouvé");
+            return;
+        }
+        
+        
+
+        /*_vertices = _mesh.vertices;
         _normals = _mesh.normals;
+        _triangles = _mesh.triangles;
+        for(int i = 0; i < _normals.Length; i++)
+            Debug.Log(_normals[i]);
+        Debug.Log("_________________________");
+        for(int i = 0; i < _vertices.Length; i++)
+            Debug.Log(_vertices[i]);
+        Debug.Log("_________________________");
+        for(int i = 0; i < _triangles.Length; i++)
+            Debug.Log(_triangles[i]);
+        Debug.Log("_________________________");
+        Debug.Log("_normals.Length : " + _normals.Length);
+        Debug.Log("_vertices.Length : " + _vertices.Length);
+        Debug.Log("_triangles.Length : " + _triangles.Length);*/
     }
 
     void Update()
---
+++ Assets/Scripts/CatmullClark/DetectFaces.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFaces : MonoBehaviour
+{
+    private float amount = 0.5f;
+    private Vector3[] surfaceNormals;
+    private Vector3[] newVertices ;
+    float maxDist = 20;
+    
+    void Start()
+    {
+        var myMesh = GetComponent<MeshFilter>().mesh;
+        var myVertices = myMesh.vertices;
+        var myTriangles = myMesh.triangles;
+        var myUV = myMesh.uv;
+        var myNormals = myMesh.normals;
+        // Set up new arrays to use with rebuilt mesh
+        newVertices = new Vector3[myTriangles.Length];
+        var newUV = new Vector2[myTriangles.Length];
+        var newNormals = new Vector3[myTriangles.Length];
+        var newTriangles = new int[myTriangles.Length];
+    
+        // Rebuild mesh so that every triangle has unique vertices
+        for (var i = 0; i < myTriangles.Length; i++) {
+            newVertices[i] = myVertices[myTriangles[i]];
+            newUV[i] = myUV[myTriangles[i]];
+            newNormals[i] = myNormals[myTriangles[i]];
+            newTriangles[i] = i;
+        }
+    
+        // Assign new mesh
+        myMesh.vertices = newVertices;
+        myMesh.uv = newUV;
+        myMesh.normals = newNormals;
+        myMesh.triangles = newTriangles;
+    
+        // Get array of surface normals for each triangle
+        surfaceNormals = new Vector3[myTriangles.Length/3];
+        var idx = 0;
+        for (var i = 0; i < surfaceNormals.Length; i++) {
+            var v0 = newVertices[idx++];
+            surfaceNormals[i] = Vector3.Cross(newVertices[idx++]-v0, newVertices[idx++]-v0).normalized;
+        }
+        
+        ExplodeMesh(amount);
+    }
+
+    void Update()
+    {
+        
+    }
+ 
+    void ExplodeMesh (float amount) {
+        var idx = 0;
+        var myMesh = GetComponent<MeshFilter>().mesh;
+        var thisVertices = myMesh.vertices;
+        for (var i = 0; i < surfaceNormals.Length; i++) {
+            thisVertices[idx] = newVertices[idx++] + surfaceNormals[i] * amount;
+            thisVertices[idx] = newVertices[idx++] + surfaceNormals[i] * amount;
+            thisVertices[idx] = newVertices[idx++] + surfaceNormals[i] * amount;
+        }
+        myMesh.vertices = thisVertices;
+    }
+}
